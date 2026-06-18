2409bc94fe8f2a377debf6b4a5d2d1cf755c86f5
MustfaAshraf <mostafa012f@gmail.com>
2026-06-18 20:43:48 +0300

    chore: add dotenv dependency and fix the database configurations

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const database = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/metaDB')
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("Database Connected Successfully...");
         
     } catch (error) {
